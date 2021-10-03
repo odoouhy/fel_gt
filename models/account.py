9ba8556161f5f662df9f094ef8dd7f0a69961543
Allan Ramirez <aramirez@exeticsoluciona.com>
2021-10-02 19:39:25 -0600

    [UPD] to the latest revision 1.5.5

--- models/account.py
+++ models/account.py
@@ -167,7 +167,7 @@ class AccountInvoice(models.Model):
         Pais = etree.SubElement(DireccionReceptor, DTE_NS+"Pais")
         Pais.text = factura.partner_id.country_id.code or 'GT'
 
-        if tipo_documento_fel not in ['NDEB', 'NCRE', 'RECI', 'NABN', 'FESP']:
+        if tipo_documento_fel not in ['NABN']:
             ElementoFrases = etree.fromstring(factura.company_id.frases_fel)
             DatosEmision.append(ElementoFrases)
 
